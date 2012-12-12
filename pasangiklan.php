0e31d0c59b7dd2e2ad288d0ce212364ad266db06
achmad.rinaldy <achmad.rinaldy@gmail.com>
2012-12-12 08:51:38 +0000

    pada usecase ini melakukan pasang iklan

---
+++ pasangiklan.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="id">
+<head>
+		
+<title>Dumbe.com</title>
+		
+      
+
+
+		<link href="Dumbe.com/global00.css" rel="stylesheet" type="text/css" media="all" />
+	
+<link rel="stylesheet" href="Dumbe.com_files/menu_sty.css" type="text/css" />
+		<script type="text/javascript" src="Dumbe.com_files/tools000.js"></script>
+		<script type="text/javascript">
+			var baseDir = '/';
+			var static_token = '14c7f7db2acbe2593ea73e8f4769bf5f';
+			var token = '3a36c0dd95ff4ad02a1c281b12466dec';
+			var priceDisplayPrecision = 0;
+		</script>
+		<script type="text/javascript" src="Dumbe.com/jquery-1.js"></script>
+		<script type="text/javascript" src="Dumbe.com_files/jquery00.js"></script>
+        
+  <script type="text/javascript" src="Dumbe.com_files/global00.htm"></script>
+
+		<link rel="alternate" type="application/rss+xml" title="Dumbe.com." href="http://www.Dumbe.com/modules/feeder/rss.php?id_category=0&amp;orderby=position&amp;orderway=DESC" />
+	    <style type="text/css">
+<!--
+.style3 {color: #FF0000}
+.style4 {
+	color: #000000;
+	font-weight: bold;
+}
+.style5 {
+	font-size: 24px;
+	font-weight: bold;
+	color: #000000;
+}
+-->
+        </style>
+</head>
+	
+	<body id="index">
+			<div id="page">
+
+			<!-- Header -->
+			<div>
+				<h1 id="logo">&nbsp;</h1>
+				<div id="header">
+				  <!-- Block currencies module -->
+<script type="text/javascript" src="Dumbe.com_files/blockcur.js"></script>
+<!-- /Block currencies module -->
+<!-- Block languages module -->
+<div id="languages_block_top"></div>
+<script type="text/javascript">
+	$('ul#first-languages li:not(.selected_language)').css('opacity', 0.3);
+	$('ul#first-languages li:not(.selected_language)').hover(function(){
+		$(this).css('opacity', 1);
+	}, function(){
+		$(this).css('opacity', 0.3);
+	});
+</script>
+<!-- /Block languages module -->
+<!-- Block permanent links module HEADER -->
+<ul id="header_links"><li id="header_link_bookmark">
+		<script type="text/javascript">writeBookmarkLink('http://www.batikdistro.com/index.php', 'BatikDistro.Com', 'bookmark');</script>
+	</li>
+</ul>
+<!-- /Block permanent links module HEADER --><!-- Block search module HEADER -->
+<div id="search_block_top">
+	<form method="get" action="http://www.Dumbe.com/search.php" id="searchbox">
+	<p>
+		<label for="search_query"><!-- image on background --></label>
+		<input type="text" id="search_query" name="search_query" value="" />
+		<input type="submit" name="submit_search" value="Search" class="button" />
+	</p>
+	</form>
+</div>
+<!-- /Block search module HEADER --><!-- Block user information module HEADER -->
+<!-- /Block user information module HEADER -->
+              </div>
+				<div class="menu bubplastic horizontal red" style="position:relative;float:left; margin-bottom:10px">
+	<ul>
+		<li class="highlight"><span class="menu_r"><a href="home.php" target="_self"><span class="menu_ar">Home</span></a></span></li>
+			<li><span class="menu_r"><a href="sejarah_batik.html" target="_self">Motor Bekas </a></span></li>
+        	<li><span class="menu_r"><a href="pengiriman_pembayaran.html" target="_self">Motor Baru </a></span></li>
+            <li><span class="menu_r"><a href="P_coba.php" target="_self">Saran</a></span></li>
+			 </ul>
+</div>
+			</div>
+
+			<div align="center">
+			  <!-- Left -->
+			  <!-- Center -->
+            </div>
+			<div align="center"></div>
+			<div id="center_column">
+	<!-- Module Editorial -->
+<div id="editorial_block_center" class="editorial_block">  		 
+	<span class="style5">PASANG IKLAN	</span>
+	<table width="640" border="1">
+      <tr>
+        <td width="121" height="32"><div align="center">
+          <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
+            <param name="movie" value="button3.swf" />
+            <param name="quality" value="high" />
+            <embed src="button3.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
+          </object>
+        </div></td>
+        <td width="121"><div align="center">
+          <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
+            <param name="movie" value="button4.swf" />
+            <param name="quality" value="high" />
+            <embed src="button4.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
+          </object>
+        </div></td>
+        <td width="120"><div align="center">
+          <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
+            <param name="movie" value="button5.swf" />
+            <param name="quality" value="high" />
+            <embed src="button5.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
+          </object>
+        </div></td>
+        <td width="116"><div align="center">
+          <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
+            <param name="movie" value="button6.swf" />
+            <param name="quality" value="high" />
+            <embed src="button6.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
+          </object>
+        </div></td>
+        <td width="128"><div align="center">
+          <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
+            <param name="movie" value="button7.swf" />
+            <param name="quality" value="high" />
+            <embed src="button7.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
+          </object>
+        </div></td>
+      </tr>
+    </table>
+	<h2 class="style3">&nbsp;</h2>
+	<div><p style="text-align: center;">&nbsp;</p>
+      <table width="70%" border="1" align="center" cellpadding="0" cellspacing="3" bordercolor="#CCCCCC">
+        <form action="load.php" method="post" enctype="multipart/form-data">
+          <tr>
+            <td width="27%"><strong><font color="#000000">Judul Iklan </font></strong></td>
+            <td width="73%"><div align="left">
+              <input name="TF1" type="text" id="TF1" />
+            </div></td>
+          </tr>
+		  <tr>
+            <td><span class="style4">Kategori</span></td>
+            <td><?
+		$kon = mysql_connect("localhost","root","") or die ("Salah Bozzz");
+		mysql_select_db("dbtgs",$kon);
+		$query = "SELECT * FROM motor";
+		$result = mysql_query($query);
+		//$conn = mysql_close();
+		?>
+                <div align="left">
+                  <select name="select" id="select">
+                    <option>--pilih--</option>
+                    <?
+	while ($rows = mysql_fetch_array ($result))
+	{
+		$a=($a==1) ? 0 : 1;
+		$col=($a==1) ? "#CCCCCC" : "#999999";
+	?>
+                    <option value="<?=$rows[it]?>" selected = "selected" >
+                    <?=$rows[motor]?>
+                    </option>
+                    <? }?>
+                  </select>
+                  </div></td>
+          </tr>
+          
+          
+          <tr>
+            <td><strong><font color="#000000">Harga</font></strong></td>
+            <td><input name="DK9" type="text" id="DK9" value="" size="50" /></td>
+          </tr>
+          <tr>
+            <td><strong><font color="#000000">Kondisi</font></strong></td>
+            <td><input name="DK10" type="text" id="DK10" value="" size="50" /></td>
+          </tr>
+          <tr>
+            <td><strong><font color="#000000">Lokasi Penjualan </font></strong></td>
+            <td><input name="DK11" type="text" id="DK11" value="" size="50" /></td>
+          </tr>
+         <tr>
+            <td><font color="#000000"><strong>Deskripsi</strong></font></td>
+            <td><p>&nbsp;</p></td>
+          </tr>
+          <tr>
+            <td><strong><font color="#000000">Upload foto </font></strong></td>
+            <td><input name="file" type="file" id="file" /></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td><input name="Upload" type="submit" id="Upload" value="Upload" /></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+        </form>
+      </table>
+      <p style="text-align: center;">&nbsp;</p>
+</div></div>
+<!-- /Module Editorial --><!-- MODULE Home Featured Products -->
+<!-- /MODULE Home Featured Products -->
+            </div>
+			<!-- Right -->
+			
+
+<!-- Footer -->
+			<div id="footer"><!-- MODULE Block various links -->
+	<td class="footer" align="center" height="25"><a href="http://www.DistroBatik.com/" title="Batik Online" class="footerlink"></a>
+    <marquee behavior="scroll">
+    <h1>www.Dumbe.com</h1> 
+    </marquee></td>
+
+
+</ul>
+<!-- /MODULE Block various links -->
+
+		<script type="text/javascript">
+			var time_start;
+			$(window).load(
+				function() {
+					time_start = new Date();
+				}
+			);
+			$(window).unload(
+				function() {
+					var time_end = new Date();
+					var pagetime = new Object;
+					pagetime.type = "pagetime";
+					pagetime.token = "Sv78l76+vkw=bJKtkXuMYqw=ik9bVSwF8es=Q3rs55VXkIE=";
+					pagetime.time = time_end-time_start;
+					$.post("/statistics.php", pagetime);
+				}
+			);
+		</script>
+		<script type="text/javascript">
+			var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+			document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+		</script>
+		<script type="text/javascript">
+			var pageTracker = _gat._getTracker("UA-9988668-1");
+			pageTracker._trackPageview();
+			
+		</script></div>
+		</div>
+		</body>
+</html>
