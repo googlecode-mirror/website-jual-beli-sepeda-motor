42600dab065891024399d43810c8aa5d249b3b4d
sealarus2@gmail.com <sealarus2@gmail.com@62fec6a6-18f9-3d68-ece7-03b0e40afcfd>
2012-12-19 08:05:59 +0000

    Tampilan Homepage untuk pelanggan

---
+++ trunk/home1.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="id">
+	<head>
+		
+		<title>Dumbe.com</title>
+		
+      
+
+
+		<link href="Dumbe.com/global00.css" rel="stylesheet" type="text/css" media="all" />
+	
+<link rel="stylesheet" href="Dumbe.com_files/menu_sty.css" type="text/css" />
+		<script type="text/javascript" src="Dumbe.com_files/tools000.js"></script>
+		<script type="text/javascript">
+			var baseDir = '/';
+			var static_token = '14c7f7db2acbe2593ea73e8f4769bf5f';
+			var token = '3a36c0dd95ff4ad02a1c281b12466dec';
+			var priceDisplayPrecision = 0;
+		</script>
+		<script type="text/javascript" src="Dumbe.com/jquery-1.js"></script>
+		<script type="text/javascript" src="Dumbe.com_files/jquery00.js"></script>
+        
+  <script type="text/javascript" src="Dumbe.com_files/global00.htm"></script>
+
+		<link rel="alternate" type="application/rss+xml" title="Dumbe.com." href="http://www.Dumbe.com/modules/feeder/rss.php?id_category=0&amp;orderby=position&amp;orderway=DESC" />
+	    <style type="text/css">
+<!--
+.style2 {
+	color: #CC9933;
+	font-style: italic;
+}
+.style3 {color: #FF0000}
+.style4 {color: #FFFFFF}
+-->
+        </style>
+</head>
+	
+	<body id="index">
+			<div id="page">
+
+			<!-- Header -->
+			<div>
+				<h1 id="logo">&nbsp;</h1>
+				<div id="header">
+				<p>&nbsp;</p>
+				  <p>&nbsp;</p>
+				  <p>	<td height="10"></td>
+	
+    <td colspan="2" class="text"><marquee behavior="alternate">welcome <?=$user?> </marquee></td>
+    <td class="text">&nbsp;</td>
+
+&nbsp;<a
+href="logout.php"><strong>LOGOUT</strong></a></font></td></p>
+				  <p>
+				  <!-- Block currencies module -->
+<script type="text/javascript" src="Dumbe.com_files/blockcur.js"></script>
+<!-- /Block currencies module -->
+<!-- Block languages module -->
+<div id="languages_block_top"></div>
+<script type="text/javascript">
+	$('ul#first-languages li:not(.selected_language)').css('opacity', 0.3);
+	$('ul#first-languages li:not(.selected_language)').hover(function(){
+		$(this).css('opacity', 1);
+	}, function(){
+		$(this).css('opacity', 0.3);
+	});
+</script>
+<!-- /Block languages module -->
+<!-- Block permanent links module HEADER -->
+<ul id="header_links"><li id="header_link_bookmark">
+		<script type="text/javascript">writeBookmarkLink('http://www.batikdistro.com/index.php', 'BatikDistro.Com', 'bookmark');</script>
+	</li>
+</ul>
+<!-- /Block permanent links module HEADER --><!-- Block search module HEADER -->
+<div id="search_block_top">
+	<form method="get" action="search.php" id="searchbox">
+	<p>
+		<label for="search_query"><!-- image on background --></label>
+		<input type="text" id="f" name="search_query" value="" />
+		<input type="submit" name="submit_search" value="Search" class="button" />
+	</p>
+	</form>
+</div>
+<!-- /Block search module HEADER --><!-- Block user information module HEADER -->
+<!-- /Block user information module HEADER -->
+              </div>
+				<div class="menu bubplastic horizontal red" style="position:relative;float:left; margin-bottom:10px">
+	              <p>&nbsp;</p>
+	              <p>&nbsp;</p>
+	              <p class="style4">=======&gt;&gt;</p>
+				</div>
+			</div>
+
+			<!-- Left -->
+			<!-- Center -->
+<div id="center_column">
+	<!-- Module Editorial -->
+<div id="editorial_block_center" class="editorial_block">
+	<a href="http://www.Dumbe.com/" title="Motor dot com">			</a>	 
+	<h2 class="style3">Selamat Datang di Dumbe.Com</h2>
+	<table width="640" border="1">
+      <tr>
+        <td width="121" height="32"><div align="center">
+            <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
+              <param name="BGCOLOR" value="" />
+              <param name="movie" value="button3.swf" />
+              <param name="quality" value="high" />
+              <embed src="button3.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
+            </object>
+        </div></td>
+        <td width="121"><div align="center">
+            <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
+              <param name="BGCOLOR" value="" />
+              <param name="movie" value="button4.swf" />
+              <param name="quality" value="high" />
+              <embed src="button4.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
+            </object>
+        </div></td>
+        <td width="120"><div align="center">
+            <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
+              <param name="BGCOLOR" value="" />
+              <param name="movie" value="button5.swf" />
+              <param name="quality" value="high" />
+              <embed src="button5.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
+            </object>
+        </div></td>
+        <td width="116"><div align="center">
+            <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
+              <param name="BGCOLOR" value="" />
+              <param name="movie" value="button6.swf" />
+              <param name="quality" value="high" />
+              <embed src="button6.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
+            </object>
+        </div></td>
+        <td width="128"><div align="center">
+            <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="22">
+              <param name="BGCOLOR" value="" />
+              <param name="movie" value="button7.swf" />
+              <param name="quality" value="high" />
+              <embed src="button7.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="22" ></embed>
+            </object>
+        </div></td>
+      </tr>
+    </table>
+	<p class="style3">&nbsp;</p>
+	<div><p style="text-align: center;"><img src="gambar/v3.jpg" alt="" width="253" height="158" /></p>
+<p style="text-align: center;"><span style="color: #ff0000;"><span style="font-size: medium;"><strong>Ayo Gabung !!!! </strong></span></span><a href="about_us.html"><span style="color: #ff0000;"><span style="font-size: medium;"><strong><span class="style2"><br />
+</span></strong></span></span></a></p>
+<p style="text-align: center;">&nbsp;</p>
+<p style="text-align: left;"><strong>Selamat Datang di toko online Dumbe.com</strong></p>
+<p align="left">Kami akan memberikan pelayanan terbaik kepada Anda.  Dengan  Motor terbaru dan Bekas dengan kualitas tinggi dengan harga  bersaing.</p>
+<p><strong>Keuntungan</strong> belanja di Motor antara lain :</p>
+<ul class="muka">
+<li><strong>Murah</strong> : dapatkan  produk tertentu dari supplier <a href="http://www.batikdistro.com/partners.php" target="_blank">mitra</a> kami. </li>
+<li><strong>Mudah</strong> :&nbsp; Anda tinggal klik dengan mouse, barang akan langsung datang ke rumah.</li>
+<li><strong>Berkualitas</strong> : Kami selalu memberikan barang dengan kualitas yang tinggi dan dari produsen-produsen terpercaya.</li>
+<li><strong>Layanan konsumen</strong> via email, telp, YM .</li>
+</ul>
+	</div></div>
+<!-- /Module Editorial --><!-- MODULE Home Featured Products -->
+<!-- /MODULE Home Featured Products -->
+            </div>
+			<!-- Right -->
+			
+
+<!-- Footer -->
+			<div id="footer"><!-- MODULE Block various links -->
+	<td class="footer" align="center" height="25"><a href="http://www.DistroBatik.com/" title="Batik Online" class="footerlink"></a>
+    <marquee behavior="scroll">
+    <h1>www.Dumbe.com</h1> 
+    </marquee></td>
+
+
+</ul>
+<!-- /MODULE Block various links -->
+
+		<script type="text/javascript">
+			var time_start;
+			$(window).load(
+				function() {
+					time_start = new Date();
+				}
+			);
+			$(window).unload(
+				function() {
+					var time_end = new Date();
+					var pagetime = new Object;
+					pagetime.type = "pagetime";
+					pagetime.token = "Sv78l76+vkw=bJKtkXuMYqw=ik9bVSwF8es=Q3rs55VXkIE=";
+					pagetime.time = time_end-time_start;
+					$.post("/statistics.php", pagetime);
+				}
+			);
+		</script>
+		<script type="text/javascript">
+			var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+			document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+		</script>
+		<script type="text/javascript">
+			var pageTracker = _gat._getTracker("UA-9988668-1");
+			pageTracker._trackPageview();
+			
+		</script></div>
+		</div>
+		</body>
+</html>
